fb4ff13f3d339358f58d9a399e3209d206b98f38
Pamela Ayres <pamayresdl@gmail.com>
2024-09-06 22:49:49 -0300

    Integração da atualização

--- app.js
+++ app.js
@@ -1,28 +1,51 @@
 function pesquisar() {
-      // Obtém a seção onde os resultados serão exibidos
-      let section = document.getElementById("resultados-pesquisa");
-             // Inicializa uma string vazia para armazenar os resultados
-      let resultados = "";
+ // Obtém a seção onde os resultados serão exibidos
+  const section = document.getElementById("resultados-pesquisa");
+      // Obtém o termo de pesquisa inserido pelo usuário 
+  let campoPesquisa = document.getElementById("campo-pesquisa").value
+// Verifica se o usuário inseriu algum termo de pesquisa
+  if(!campoPesquisa){
+    // Se não houver termo, exibe uma mensagem informativa
+    section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar algo relacionado ao dorama.</p>"
+    return
+  }
+    // Inicializa uma string para armazenar os resultados da pesquisa 
+  campoPesquisa = campoPesquisa.toLowerCase()
+  let resultados = "";
+  let titulo = "";
+  let sinopse = "";
+  let tags = "";
+ // Itera sobre cada dado (dorama) no conjunto de dados
+    for (let dado of dados) {
+      // Converte as propriedades do dado para minúsculas para facilitar a comparação
+      titulo = dado.titulo.toLowerCase()
+      sinopse = dado.sinopse.toLowerCase()
+      tags = dado.tags.toLocaleLowerCase()
+      
+   // Verifica se o termo de pesquisa está presente no título, sinopse ou tags
+      if (titulo.includes(campoPesquisa) || sinopse.includes(campoPesquisa)|| tags.includes(campoPesquisa)) {
+        // Se o termo foi encontrado, cria um novo elemento HTML para exibir o resultado
+        resultados += `
+        <div class="item-resultado">
+        <img src="${dado.imagem}" alt="Capa dorama">
+        <div>
+          <h2> 
+            <a href="${dado.link}" target="_blank">Dorama Link -  ${dado.titulo}</a>
+          </h2>
+          <p>${dado.sinopse}</p>
+          </div>
+          
+        </div>
+        `;
+      }
     
-      // Itera sobre os dados e constrói o HTML para cada resultado
-      for (let dado of dados) {
-        // Cria um novo elemento div para cada resultado
-        resultados += `
-          <div class="item-resultado">
-            <h2>
-             <a href=${dado.titulo} target="_blank"></a>
-            </h2>
-            <div class="container">
-              <img src=${dado.imagem} alt="Capa dorama" class="imagem"> 
-                <div class="texto">
-                <p>${dado.descricao}</p> 
-                <a href=${dado.link} target="_blank">Dorama Link - Pousando no Amor</a>
-            </div>
-          </div>
-           </div>
-        `;
-      }
-    section.innerHTML = resultados
-
-
+    
+  }
+// Verifica se algum resultado foi encontrado
+  if(!resultados){
+    // Se não houver resultados, exibe uma mensagem informativa
+    resultados = "<p>Nada foi encontrado</p>"
+  }
+// Atualiza o conteúdo da seção com os resultados da pesquisa
+  section.innerHTML=resultados;
 }
